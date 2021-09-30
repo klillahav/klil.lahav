efac768378e1482428d29bfb74b5d3dcd077d819
klillahav <88575542+klillahav@users.noreply.github.com>
2021-09-30 21:32:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const sql = require("./db.js");
+const sql = require("./db");
 
 // parse requests of contenttype: application/json
 app.use(bodyParser.json());
